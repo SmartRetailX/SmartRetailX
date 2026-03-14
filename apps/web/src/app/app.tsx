49afc102acad487b821693d5c5f4ccbfe5db4a2b
Shanelka Pramuditha <99pramuditha@gmail.com>
2026-03-14 22:19:38 +0530

    Refactor code structure for improved readability and maintainability

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from "./nx-welcome";
+import '../styles.css';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="web"/>
+    </div>
+  );
+}
+
+export default App;
+
+
