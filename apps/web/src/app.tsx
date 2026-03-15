6e1107964467b50e4d9440bea81b57b8511213df
Shanelka Pramuditha <99pramuditha@gmail.com>
2026-03-16 00:37:53 +0530

    Refactor code structure for improved readability and maintainability

---
+++ apps/web/src/app.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+
+import '@/styles/globals.css';
+
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+import { router } from './router';
+
+export function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <TanStackRouterDevtools router={router} />
+    </>
+  );
+}
+
+export default App;
