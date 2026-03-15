14118c81dd62eedc58e821cc5482c56db34c5148
Shanelka Pramuditha <99pramuditha@gmail.com>
2026-03-16 01:24:53 +0530

    feat: add authentication hook and full-screen loader component

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,17 +1,23 @@
-import { Button } from '@/components/ui/button';
-
 import '@/styles/globals.css';
 
+import { useAuth } from '@/hooks';
+import { router } from '@/router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import { router } from './router';
+import { FullScreenLoader } from '@/components/system/loaders/full-screen';
 
 export function App() {
+  const auth = useAuth();
+
+  if (auth.isPending || auth.isRefetching) {
+    return <FullScreenLoader />;
+  }
+
   return (
     <>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} context={auth} />
       <TanStackRouterDevtools router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </>
